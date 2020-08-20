eff78c7400b3fcb9baaee874498e5ae7d038a1f8
João Xavier <jvitorxavier@gmail.com>
2020-08-20 18:17:45 -0300

    Adding menu.

--- currencyConverter.py
+++ currencyConverter.py
@@ -16,7 +16,14 @@ class Currency:
     quotation = json.loads(response.text)
     return quotation['rates'][self.outcome]
 
-def menu_loop():
+menu = 0
+
+while menu != 4:
+  menu = int(input("1 - Cotação do dólar/n2 - Conversão real para euro/n3 - Conversão euro para dólar/n4 - sair"))
+  if menu == 1:
+    print("O valor atual do dólar em reais é: ",get_quotation('BRL', 'USD'))
+
+""" def menu_loop():
   # income = 'BRL'
   # outcome = 'USD'
   cur = Currency()
@@ -28,8 +35,11 @@ def menu_loop():
     if num == 1:
       income = 'BRL'
       outcome = 'USD'
-      out_rate = cur.get_quotation(income, outcome)  
+      out_rate = cur.get_quotation(income, outcome)
+      print("{} to {}: {}".format(income, outcome, out_rate))
+
     elif num == 2:
+      income = ''
     
     elif num == 4:
       print("Programa finalizado")
@@ -41,4 +51,4 @@ if __name__ == "__main__":
   cur = Currency(income)
   out_rate = cur.get_quotation(outcome)
 
-  print("{} to {}: {}".format(income, outcome, out_rate))
+  print("{} to {}: {}".format(income, outcome, out_rate)) """
