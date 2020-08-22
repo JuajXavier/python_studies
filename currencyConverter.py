eacb9d4eb5173757467409ed9816dcd053465513
João Xavier <jvitorxavier@gmail.com>
2020-08-21 21:28:01 -0300

    Python class activity done.

--- currencyConverter.py
+++ currencyConverter.py
@@ -27,25 +27,14 @@ while menu != 4:
   print(f"1 - Cotação do dólar\n2 - Conversão real para euro\n3 - Conversão euro para dólar\n4 - sair")
   try:
     menu = int(input("Digite uma opção: "))
-  # if menu == 1:
-  #   rate = currency.get_quotation('USD', 'BRL')
-  #   print("O valor atual do dólar em reais é: {}".format(rate))
-  # elif menu == 2:
-  #   rate = currency.get_quotation('EUR', 'BRL')
-  #   print("O valor atual do euro em reais é: {}".format(rate))
-  # elif menu == 3:
-  #   rate = currency.get_quotation('EUR', 'USD')
-  #   print("O valor atual do euro em dólar é: {}".format(rate))
-  # else:
-  #   print("Não é um número válido")
     if menu == 1:
       income = 'USD'
       outcome = 'BRL'
       rate = currency.get_quotation(income, outcome)
       print("O valor atual do {} em {} é: {}".format(income, outcome, rate))
     elif menu == 2:
-      income = 'EUR'
-      outcome = 'BRL'
+      income = 'BRL'
+      outcome = 'EUR'
       rate = currency.get_quotation(income, outcome)
       print("O valor atual do {} em {} é: {}".format(income, outcome, rate))
       value = int(input("Digite um valor: "))
@@ -56,8 +45,11 @@ while menu != 4:
       outcome = 'USD'
       rate = currency.get_quotation(income, outcome)
       print("O valor atual do {} em {} é: {}".format(income, outcome, rate))
+      value = int(input("Digite um valor: "))
+      currencyExchange = value * rate
+      print("{} {} é igual a {:.2f} {}".format(value, income, currencyExchange, outcome))
     else:
       print("Não é um número válido")
 
   except ValueError:
-    print("Não é um número")
+    print("Não é um número ou valor válido")
